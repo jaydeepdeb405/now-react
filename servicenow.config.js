b61c4bc20123d01838ad62a50de41a01cafff49f
Jaydeep Deb <jaydeep.jaydeep.deb@gmail.com>
2020-07-28 22:38:21 +0530

    Cleared all configs

--- servicenow.config.js
+++ servicenow.config.js
@@ -3,33 +3,33 @@ module.exports = {
      * ServiceNow instance URL
      * Provide url in this format 'https://dev99999.service-now.com/' (Always prefix with https://)
      */
-    HOST                        : 'https://dev71980.service-now.com',   
+    HOST                        : '',   
     /**
      * ServiceNow instance username
      * This will be used as credential for REST in DEVELOPMENT mode
      */      
-    USERNAME                    : 'jaydeepdeb',   
+    USERNAME                    : '',   
     /**
      * ServiceNow instance password
      * This will be used as credential for REST in DEVELOPMENT mode
      */ 
-    PASSWORD                    : 'India@123',  
+    PASSWORD                    : '',  
     /**
      * ServiceNow application scope
      * Scope is 'global' for Global application & 'x_namespace_applicationname' for scoped applications
      * Leaving this empty auto defaults to Global application
      */ 
-    APP_SCOPE                   : 'global',
+    APP_SCOPE                   : '',
     /**
      * Base URL for the React application
      * This is usually /now/appname for global & /x/namespace/reactappname for scoped applications
      */ 
-    APP_URL_BASE                : '/now/first_react_app',  
+    APP_URL_BASE                : '',  
     /**
      * sys_id of ServiceNow UX source code
      * This can be found on the starter page after creating the React app in ServiceNow
      */  
-    SCRIPT_RECORD_ID            : '323c9b510792501037c7f2ae7c1ed0ab',
+    SCRIPT_RECORD_ID            : '',
     /**
      * ServiceNow Image overwrite setting 
      * If true, it will update/overwrite the existing images with the same name in the instance, happens on every 'npm run build'
